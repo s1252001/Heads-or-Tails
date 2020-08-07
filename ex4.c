a9c4a27fd447ede317484454ceb26071cad716cc
s1252001 <56055863+s1252001@users.noreply.github.com>
2020-08-07 14:44:36 +0900

    HoT: Initial revision

---
+++ ex4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+
+  int i,s,head = 0,tail = 0;
+
+  printf("Tossing a coin...\n");
+
+  srand(time(NULL));
+
+  for(i = 1; i < 6; i++){
+
+    s = rand()%2;
+
+    if(s == 0){
+
+      printf("Round %d: Heads\n",i);
+
+      head++;
+
+    }
+
+    else {
+
+      printf("Round %d: Tails\n",i);
+
+      tail++;
+
+    }
+
+    if(head == 2 || tail == 2)break;
+
+  }
+
+  printf("Heads: %d, Tails: %d\n",head,tail);
+
+
+  return 0;
+
+}
